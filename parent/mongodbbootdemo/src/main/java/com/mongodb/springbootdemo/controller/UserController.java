f1b70517600b098a1878d128ec878b24a472ff9d
chenpeng <572769918>
2018-08-05 19:23:41 +0800

    注入对象

--- parent/mongodbbootdemo/src/main/java/com/mongodb/springbootdemo/controller/UserController.java
+++ parent/mongodbbootdemo/src/main/java/com/mongodb/springbootdemo/controller/UserController.java
@@ -1,12 +1,22 @@
 package com.mongodb.springbootdemo.controller;
 
+import com.mongodb.springbootdemo.dao.UserDao;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  * @author : chenpeng
  * @date : 2018-08-05 19:03
  */
 @Controller
+@RequestMapping(value = "/index")
 public class UserController {
+  @Autowired
+  private UserDao userRepository;
 
+  @Autowired
+  private MongoTemplate mongoTemplate;
 }
