dd00147c84d2eebeb3060dcb513791c246402b60
chenpeng <572769918>
2018-08-05 15:14:12 +0800

    创建UserDao接口

---
+++ parent/springboot/src/main/java/com/mongodb/springboot/dao/UserDao.java
@@ -0,0 +1,15 @@
+package com.mongodb.springboot.dao;
+
+import com.mongodb.springboot.entity.UserEntity;
+
+/**
+ * @author : chenpeng
+ * @date : 2018-08-05 15:12
+ */
+public interface UserDao  {
+  void saveUser(UserEntity user);
+  UserEntity findUserByUserName(String userName);
+  int updateUser(UserEntity user);
+  void deleteUserById(Long id);
+
+}
