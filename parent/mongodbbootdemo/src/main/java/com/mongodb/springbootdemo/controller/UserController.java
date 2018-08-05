e181cca14fae0c9e207ab3f35fa344a68cccc9c4
chenpeng <572769918>
2018-08-05 19:28:28 +0800

    首页,登录接口

--- parent/mongodbbootdemo/src/main/java/com/mongodb/springbootdemo/controller/UserController.java
+++ parent/mongodbbootdemo/src/main/java/com/mongodb/springbootdemo/controller/UserController.java
@@ -1,10 +1,13 @@
 package com.mongodb.springbootdemo.controller;
 
 import com.mongodb.springbootdemo.dao.UserDao;
+import com.mongodb.springbootdemo.entity.User;
 
+import javax.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -19,4 +22,23 @@ public class UserController {
 
   @Autowired
   private MongoTemplate mongoTemplate;
+
+  @RequestMapping(value = "/index")
+  public String index(Model model, HttpServletRequest request) {
+
+    User user = (User) request.getAttribute("user");
+
+    if (user == null) {
+      //  System.out.println(user);
+      User usero = new User("##errors##", "1234");
+      model.addAttribute("user", usero);
+    }
+
+    return "index";
+  }
+
+  @RequestMapping(value = "/oplogin")
+  public String oplogin() {
+    return "login";
+  }
 }
