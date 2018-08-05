562123f91bac34efa1dde92df94ca178374f34a0
chenpeng <572769918>
2018-08-05 19:36:01 +0800

    用户登录接口

--- parent/mongodbbootdemo/src/main/java/com/mongodb/springbootdemo/controller/UserController.java
+++ parent/mongodbbootdemo/src/main/java/com/mongodb/springbootdemo/controller/UserController.java
@@ -38,10 +38,43 @@ public class UserController {
     return "index";
   }
 
- //登录界面
+  //登录界面
   @RequestMapping(value = "/oplogin")
   public String oplogin() {
     return "login";
   }
 
+  /**
+   * 登录
+   * @param request is not null
+   * @return
+  */
+  @RequestMapping(value = "/login")
+ public String login(HttpServletRequest request) {
+    //获取用户和密码
+    String username = request.getParameter("username");
+    String password = request.getParameter("password");
+
+
+    //根据用户名查询，用户是否存在
+    User user = userRepository.findByUsername(username);
+    //如果存在
+    if (user != null) {
+      if (password.equals(user.getPassword())) {
+        //如果密码正确
+        //将用户信息放入到会话中...
+        request.setAttribute("user", user);
+        return "index";
+      } else {
+        //如果密码错误
+        System.out.println("密码错误");
+        return "ferror";
+      }
+    } else {
+      //如果不存在，代码邮箱和密码输入有误
+      System.out.println("用户不存在");
+      return "ferror";
+    }
+
+  }
 }
