7fb1842354c313fd5a2fd289948ee31191311670
chenpeng <572769918>
2018-08-05 15:12:12 +0800

    创建User类

---
+++ parent/springboot/src/main/java/com/mongodb/springboot/entity/UserEntity.java
@@ -0,0 +1,47 @@
+package com.mongodb.springboot.entity;
+
+import java.io.Serializable;
+
+/**
+ * @author : chenpeng
+ * @date : 2018-08-05 14:59
+ */
+public class UserEntity implements Serializable {
+  private static final long serialVersionUID = -3258839839160856613L;
+  private Long id;
+  private String userName;
+  private String passWord;
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getUserName() {
+    return userName;
+  }
+
+  public void setUserName(String userName) {
+    this.userName = userName;
+  }
+
+  public String getPassWord() {
+    return passWord;
+  }
+
+  public void setPassWord(String passWord) {
+    this.passWord = passWord;
+  }
+
+  @Override
+  public String toString() {
+    return "UserEntity{"
+              +  "id=" + id
+              + ", userName='" + userName + '\''
+              +  ", passWord='" + passWord + '\''
+              + '}';
+  }
+}
